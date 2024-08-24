3c40a7e7113f04b98fed888bd7d13471cbc4f82f
ManishSharma1609 <104347990+ManishSharma1609@users.noreply.github.com>
2024-08-24 16:02:03 +0000

    airflow code added

--- airflow/dags/training_pipeline.py
+++ airflow/dags/training_pipeline.py
@@ -3,3 +3,44 @@ from textwrap import dedent
 import json
 from airflow import DAG
 from airflow.operators.python import PythonOperator 
+import pendulum
+from src.pipeline.training_pipeline import TrainingPipeline
+
+training_pipeline = TrainingPipeline()
+
+with DAG(
+    "Gemstone_training_pipeline",
+    default_args={"retries":2},
+    description="Diamong price prediction training pipeline",
+    schedule="@hourly",
+    start_date=pendulum.datetime(2024, 8, 18, tz="UTC"),
+    catchup=False,
+    tags=["Machine_learing", "Classification", "Gemstone"]
+) as dag:
+
+    dag.doc_md = __doc__
+
+    def data_ingestion(**kwargs):
+        ti = kwargs["ti"]
+        train_data_path, test_data_path = training_pipeline.start_data_Ingestion()
+        ti.xcom_push("data_ingestion_artifacts", {"train_data_path" : train_data_path, "test_data_path": test_data_path })
+
+    def data_transformation(**kwargs):
+        ti = kwargs["ti"]
+        data_ingestion_artifact = ti.xcom_pull(task_ids = "data_ingestion", key = "data_ingestion_artifacts")
+        train_arr, test_arr = training_pipeline.start_data_transformation(data_ingestion_artifact["train_data_path"], data_ingestion_artifact["test_data_path"])
+        train_arr = train_arr.tolist()
+        test_arr = test_arr.tolist()
+        ti.xcom_push("data_transformation_artifact", {"train_arr":train_arr, "test_arr": test_arr})
+
+    def model_trainer(**kwargs):
+        ti = kwargs["ti"]
+        data_transformation_artifacts = ti.xcom_pull(task_ids = "data_transformation", key = "data_transformation_artifact")
+        import numpy as np 
+        train_arr = np.array(data_transformation_artifacts["train_arr"])
+        test_arr = np.array(data_transformation_artifacts["test_arr"])
+        training_pipeline.start_model_training(train_arr, test_arr)
+
+    def push_to_s3(**kwargs):
+        import os
+        bucket_name = os.getenv("BUCKET_NAME")
--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -9,18 +9,54 @@ from src.components.data_transformation import dataTransformation
 from src.components.model_trainer import modelTrainer
 from src.components.model_evaluation import modelEvaluation
 
-obj = dataIngestion()
+# obj = dataIngestion()
 
-train_data_path,test_data_path=obj.initiate_data_ingestion()
+# train_data_path,test_data_path=obj.initiate_data_ingestion()
 
-data_Transformation  = dataTransformation()
+# data_Transformation  = dataTransformation()
 
-train_arr,test_arr = data_Transformation.initiate_data_transformation(train_data_path, test_data_path)
+# train_arr,test_arr = data_Transformation.initiate_data_transformation(train_data_path, test_data_path)
 
-model_Trainer = modelTrainer()
+# model_Trainer = modelTrainer()
 
-model_Trainer.initiate_model_training(train_arr, test_arr)
+# model_Trainer.initiate_model_training(train_arr, test_arr)
 
-model_evaluation = modelEvaluation()
+# model_evaluation = modelEvaluation()
 
-model_evaluation.initiate_model_evaluation(train_arr, test_arr)
+# model_evaluation.initiate_model_evaluation(train_arr, test_arr)
+
+class TrainingPipeline:
+    def start_data_Ingestion(self):
+        try:
+            data_Ingestion = dataIngestion()
+            train_data_path, test_data_path = data_Ingestion.initiate_data_ingestion()
+            return train_data_path, test_data_path
+        except Exception as e:
+            logging.info(e, sys)
+
+    def start_data_transformation(self, train_data_path, test_data_path):
+        try:
+            data_Transformation = dataTransformation()
+            train_arr, test_arr = dataTransformation.initiate_data_transformation(train_data_path, test_data_path)
+            return train_arr, test_arr
+        
+        except Exception as e:
+            logging.info(e, sys)
+
+    def start_model_training(self, train_arr, test_arr):
+        try:
+            model_trainer = modelTrainer()
+            model_trainer.initiate_model_training(train_arr, test_arr)
+        
+        except Exception as e:
+            logging.info(e, sys)
+
+    def start_training(self):
+        try:
+            train_data_path, test_data_path = self.start_data_Ingestion()
+            train_arr, test_arr = self.start_data_transformation(train_data_path, test_data_path)
+            self.start_model_training(train_arr, test_arr)
+
+        except Exception as e:
+            logging.info(e,sys)
+
